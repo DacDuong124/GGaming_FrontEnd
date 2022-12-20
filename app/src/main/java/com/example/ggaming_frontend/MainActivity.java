28c0d68902a9e7305392c57ad4476786db3aeb62
Hoang Minh Quan <64230078+minhquan1701@users.noreply.github.com>
2022-12-20 12:16:35 +0700

    switching between screens from menu bar item selected

--- app/src/main/java/com/example/ggaming_frontend/MainActivity.java
+++ app/src/main/java/com/example/ggaming_frontend/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.ggaming_frontend;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -12,7 +16,8 @@ import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
-    BottomNavigationView navigationView;
+    private BottomNavigationView navigationView;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,23 +25,64 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initComponents();
+
     }
 
     private void initComponents() {
+        viewPager = findViewById(R.id.view_pager);
         navigationView = findViewById(R.id.bottom_nav);
         navigationView.setOnItemSelectedListener(item -> {
             switch (item.getItemId()){
                 case R.id.action_home:
-                    Toast.makeText(this ,"This is home", Toast.LENGTH_SHORT).show();
+                    viewPager.setCurrentItem(0);
                     break;
                 case R.id.action_my_games:
-                    Toast.makeText(this ,"This is my games", Toast.LENGTH_SHORT).show();
+                    viewPager.setCurrentItem(1);
                     break;
                 case R.id.action_cart:
-                    Toast.makeText(this,"This is cart", Toast.LENGTH_SHORT).show();
+                    viewPager.setCurrentItem(2);
                     break;
             }
             return true;
+        });
+
+        setSwitchMenuItemScreen();
+
+    }
+
+    private void setSwitchMenuItemScreen(){
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        viewPager.setAdapter(viewPagerAdapter);
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                switch (position){
+                    case 0:
+                        navigationView.getMenu().findItem(R.id.action_home).setChecked(true);
+
+
+                    case 1:
+                        navigationView.getMenu().findItem(R.id.action_my_games).setChecked(true);
+
+
+                    case 2:
+                        navigationView.getMenu().findItem(R.id.action_cart).setChecked(true);
+
+
+                }
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
         });
 
     }
