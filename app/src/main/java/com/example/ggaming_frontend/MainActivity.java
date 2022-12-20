98391519f97c854fe7c36ff03dac535b40629251
Hoang Minh Quan <64230078+minhquan1701@users.noreply.github.com>
2022-12-20 11:28:36 +0700

    bottom navigation bar

--- app/src/main/java/com/example/ggaming_frontend/MainActivity.java
+++ app/src/main/java/com/example/ggaming_frontend/MainActivity.java
@@ -1,18 +1,44 @@
 package com.example.ggaming_frontend;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //blah blah blah blah
-        //hyeon-bi
+        initComponents();
+    }
+
+    private void initComponents() {
+        navigationView = findViewById(R.id.bottom_nav);
+        navigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.action_home:
+                    Toast.makeText(this ,"This is home", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.action_my_games:
+                    Toast.makeText(this ,"This is my games", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.action_cart:
+                    Toast.makeText(this,"This is cart", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+            return true;
+        });
+
     }
 
 }
