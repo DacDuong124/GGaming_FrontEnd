136f179f6f2122b7ec0ca2fab13a7a254cde53bf
Thien Van <[200~ttv.uniz.b4m7t@gmail.com>
2023-01-18 06:22:16 +0700

    finish login activity

---
+++ app/src/main/java/com/example/ggaming_frontend/Constants.java
@@ -0,0 +1,12 @@
+package com.example.ggaming_frontend;
+
+public class Constants {
+    //All Toast messages being used
+    public static class ToastMessage {
+        public static final String emptyInputError = "Please enter your email and password for authentication.";
+        public static final String signInSuccess = "Sign in successfully!";
+        public static final String signInFailure = "Invalid email/password!";
+        public static final String registerSuccess = "Successfully registered";
+        public static final String registerFailure = "Authentication failed, email must be unique and has correct form!";
+    }
+}
