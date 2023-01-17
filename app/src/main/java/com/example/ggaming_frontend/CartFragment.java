43cfe3c026378785d0c397b736ba6846943fc083
Hoang Minh Quan <64230078+minhquan1701@users.noreply.github.com>
2023-01-17 21:09:52 +0700

    fix cart dialog

--- app/src/main/java/com/example/ggaming_frontend/CartFragment.java
+++ app/src/main/java/com/example/ggaming_frontend/CartFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -30,11 +31,15 @@ public class CartFragment extends Fragment{
     View parent;
     RecyclerView mygame_list;
     Dialog point_dialog;
+    Button point_button;
+    Button card_button;
+
     Dialog card_dialog;
 
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         loadGames();
+
     }
 
     private void loadGames() {
@@ -81,6 +86,22 @@ public class CartFragment extends Fragment{
                              Bundle savedInstanceState) {
         parent = inflater.inflate(R.layout.fragment_cart, container, false);
         mygame_list = parent.findViewById(R.id.select_list);
+        point_button = parent.findViewById(R.id.Point_btn);
+        card_button = parent.findViewById(R.id.Card_btn);
+
+        point_button.setOnClickListener(v -> {
+            point_dialog = new Dialog(CartFragment.this.getContext());
+            point_dialog.setContentView(R.layout.point_dialog);
+            point_dialog.show();
+        });
+
+        card_button.setOnClickListener(v -> {
+            card_dialog = new Dialog(CartFragment.this.getContext());
+            card_dialog.setContentView(R.layout.card_dialog);
+            card_dialog.show();
+        });
+
+
         return parent;
     }
 
@@ -99,15 +120,15 @@ public class CartFragment extends Fragment{
             outRect.bottom = verticalSpaceHeight;
         }
     }
-    public void onClickPoint(View view) {
-       point_dialog = new Dialog(CartFragment.this.getContext());
-       point_dialog.setContentView(R.layout.point_dialog);
-       point_dialog.show();
-    }
-
-    public void onClickCard(View view) {
-        card_dialog = new Dialog(CartFragment.this.getContext());
-        card_dialog.setContentView(R.layout.card_dialog);
-        card_dialog.show();
-    }
+//    public void onClickPoint(View view) {
+//       point_dialog = new Dialog(CartFragment.this.getContext());
+//       point_dialog.setContentView(R.layout.point_dialog);
+//       point_dialog.show();
+//    }
+//
+//    public void onClickCard(View view) {
+//        card_dialog = new Dialog(CartFragment.this.getContext());
+//        card_dialog.setContentView(R.layout.card_dialog);
+//        card_dialog.show();
+//    }
 }
