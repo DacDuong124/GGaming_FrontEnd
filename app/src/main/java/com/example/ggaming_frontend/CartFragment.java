dcd076caa0cdbe4b347200313a1e08cb45bdd25b
Thien Van <[200~ttv.uniz.b4m7t@gmail.com>
2023-01-20 07:33:56 +0700

    Random 3 games for cart, calculate total price

--- app/src/main/java/com/example/ggaming_frontend/CartFragment.java
+++ app/src/main/java/com/example/ggaming_frontend/CartFragment.java
@@ -1,5 +1,6 @@
 package com.example.ggaming_frontend;
 
+import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.graphics.Rect;
 import android.os.Bundle;
@@ -14,6 +15,7 @@ import android.widget.ImageButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -22,7 +24,13 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.ggaming_frontend.components.GameCard;
+import com.example.ggaming_frontend.models.Category;
 import com.example.ggaming_frontend.models.Game;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -31,6 +39,11 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 
 public class CartFragment extends Fragment{
@@ -43,49 +56,107 @@ public class CartFragment extends Fragment{
 
     Dialog card_dialog;
 
+    TextView totalPriceTextView;
+
+    private ArrayList<Category> categories = new ArrayList<>();
+
+    private ArrayList<Game> games = new ArrayList<>();
+
+    private ArrayList<Game> randomGames = new ArrayList<>();
+
+    private FirebaseFirestore db;
+
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-//        loadGames();
-
+        db = FirebaseFirestore.getInstance();
+        fetchCategories();
     }
 
-//    private void loadGames() {
-//        LinearLayoutManager layoutManager= new LinearLayoutManager(CartFragment.this.getContext(),LinearLayoutManager.VERTICAL, false);
-//        mygame_list.setLayoutManager(layoutManager);
-//        mygame_list.addItemDecoration(new CartFragment.VerticalSpaceItemDecoration(16) );
-//
-//        try {
-//            ArrayList<Game> topSellerGameArray = new ArrayList<Game>();
-//            JSONObject obj = new JSONObject(loadJSONFromAsset("games.json"));
-//            JSONArray GamesJsonArray = obj.getJSONArray("games");
-//
-//            for (int i = 0; i < GamesJsonArray.length(); i++) {
-//                Game GameObj = new Game(GamesJsonArray.getJSONObject(i));
-//                topSellerGameArray.add(GameObj);
-//
-//            }
-//            GameCard gameCardView = new GameCard(CartFragment.this.getContext() , topSellerGameArray);
-//            mygame_list.setAdapter(gameCardView);
-//        } catch (JSONException e) {
-//            e.printStackTrace();
-//        }
-//    }
+    private void renderGames() {
+        LinearLayoutManager layoutManager= new LinearLayoutManager(CartFragment.this.getContext(),LinearLayoutManager.VERTICAL, false);
+        mygame_list.setLayoutManager(layoutManager);
+        mygame_list.addItemDecoration(new CartFragment.VerticalSpaceItemDecoration(16) );
+        randomGames = new ArrayList<>(pickNRandomElements(games, 3));
+        GameCard gameCardView = new GameCard(CartFragment.this.getContext() , randomGames);
+        mygame_list.setAdapter(gameCardView);
+        calculateAndRenderTotalPrice();
+    }
 
-    private String loadJSONFromAsset(String fileName) {
-        String json = null;
-        try {
-            InputStream is = getResources().getAssets().open(fileName);
-
-            int size = is.available();
-            byte[] buffer = new byte[size];
-            is.read(buffer);
-            is.close();
-            json = new String(buffer, "UTF-8");
-        } catch (IOException ex) {
-            ex.printStackTrace();
-            return null;
+    @SuppressLint("SetTextI18n")
+    private void calculateAndRenderTotalPrice () {
+        float totalPrice = 0.0F;
+        for (Game game : randomGames) {
+            totalPrice += Float.parseFloat(game.getPrice());
+            System.out.println("ccc: " + totalPrice);
         }
-        return json;
+
+        totalPriceTextView.setText(Float.toString(totalPrice));
+    }
+
+    public static <E> List<E> pickNRandomElements(List<E> list, int n, Random r) {
+        int length = list.size();
+
+        if (length < n) return null;
+
+        //We don't need to shuffle the whole list
+        for (int i = length - 1; i >= length - n; --i)
+        {
+            Collections.swap(list, i , r.nextInt(i + 1));
+        }
+        return list.subList(length - n, length);
+    }
+
+    public static <E> List<E> pickNRandomElements(List<E> list, int n) {
+        return pickNRandomElements(list, n, ThreadLocalRandom.current());
+    }
+
+    private void fetchGames() {
+        db.collection(Constants.FSGames.gamesCollection)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                ArrayList<Category> curCategories = new ArrayList<>();
+                                ArrayList<String> categoriesIdList = (ArrayList<String>) document.get(Constants.FSGames.categoriesField);
+                                for (String categoryId : categoriesIdList) {
+                                    Category category = Category.getCategoryById(categories, categoryId);
+                                    curCategories.add(category);
+                                }
+                                Game game = new Game(
+                                        Objects.requireNonNull(document.get(Constants.FSGames.titleField)).toString(), document.get(Constants.FSGames.priceField).toString(), curCategories, document.get(Constants.FSGames.descField).toString(), document.get(Constants.FSGames.imgField).toString());
+                                games.add(game);
+                            }
+                            renderGames();
+                        } else {
+                            Toast.makeText(getActivity(),
+                                    "Failed to get list of categories!",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+    private void fetchCategories() {
+        db.collection(Constants.FSCategories.categoriesCollection)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Category category = document.toObject(Category.class);
+                                categories.add(category);
+                            }
+                            fetchGames();
+                        } else {
+                            Toast.makeText(getActivity(),
+                                    "Failed to get list of categories!",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
     }
 
     @Override
@@ -95,6 +166,7 @@ public class CartFragment extends Fragment{
         mygame_list = parent.findViewById(R.id.select_list);
         point_button = parent.findViewById(R.id.Point_btn);
         card_button = parent.findViewById(R.id.Card_btn);
+        totalPriceTextView = parent.findViewById(R.id.cartTotalPriceTextView);
 
         point_button.setOnClickListener(v -> {
             point_dialog = new Dialog(CartFragment.this.getContext());
